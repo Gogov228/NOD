809e8e574bf03ba6d3cfc06bd4e0697ea3dd4bf2
Nickolay <voene98@gmail.com>
2018-10-12 00:26:57 +1000

    screen right-left

--- app/src/main/java/com/example/nickolay/nod/MainActivity.java
+++ app/src/main/java/com/example/nickolay/nod/MainActivity.java
@@ -10,14 +10,29 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private static final String KEY_NUM="NUM";
+    private static final String KEY_NUM2 = "NUM2";
+    int a,b;
+
     public static final String EXTRA_MESSAGE = "com.example.nickolay.MESSAGE";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    private static final String TAG ="myLogs";
 
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "Магический поворот экрана");
+        outState.putInt(KEY_NUM, a);
+        outState.putInt(KEY_NUM, b);
+    }
+
+    private static final String TAG ="myLogs";
 
     public void onButtonClick(View v){
 
@@ -26,21 +41,22 @@ public class MainActivity extends AppCompatActivity {
         EditText num2=(EditText)findViewById(R.id.num2);
         TextView Result=(TextView)findViewById(R.id.Result);
 
-        int a=Integer.parseInt(num1.getText().toString());
-        int b=Integer.parseInt(num2.getText().toString());
-        Log.i(TAG,"Находим НОД чисел "+a+" "+b);
-        try {
-            while(a!=0&&b!=0) {
-                if(a>b)
-                    a%=b;
-                else
-                    b%=a;
+            a = Integer.parseInt(num1.getText().toString());
+            b = Integer.parseInt(num2.getText().toString());
+            Log.d(TAG, "Найдём НОД чисел "+a+" "+b);
+            try {
+                while(a!=0&&b!=0) {
+                    if(a>b)
+                        a%=b;
+                    else
+                        b%=a;
 
+                }
+                Log.i(TAG,"Результат: "+(a+b));
+            } catch (Exception e) {
+                Log.d(TAG,"Ошибка в вычислении, хотя хз какая может быть ошибка)");
             }
-            Log.i(TAG,"Результат: "+(a+b));
-        } catch (Exception e) {
-            Log.d(TAG,"Ошибка в вычислении, хотя хз какая может быть ошибка)");
-        }
+
 
         String message=Integer.toString(a+b);
         Result.setText(Integer.toString(a+b));
