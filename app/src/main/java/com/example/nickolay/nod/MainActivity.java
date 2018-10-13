168cdd7b3c46f7f8cdca60401230e484cff88a9d
Nickolay <voene98@gmail.com>
2018-10-14 02:10:23 +1000

    History v0.8

--- app/src/main/java/com/example/nickolay/nod/MainActivity.java
+++ app/src/main/java/com/example/nickolay/nod/MainActivity.java
@@ -8,13 +8,15 @@ import android.view.Display;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.util.LinkedList;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private static final String KEY_NUM="NUM";
+    private static final String KEY_NUM = "NUM";
     private static final String KEY_NUM2 = "NUM2";
-    int a,b;
+    int a, b, nod;
+    NOD result = new NOD();
 
     public static final String EXTRA_MESSAGE = "com.example.nickolay.MESSAGE";
 
@@ -29,40 +31,57 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
         Log.d(TAG, "Магический поворот экрана");
         outState.putInt(KEY_NUM, a);
-        outState.putInt(KEY_NUM, b);
+        outState.putInt(KEY_NUM2, b);
     }
 
-    private static final String TAG ="myLogs";
+    private static final String TAG = "myLogs";
 
-    public void onButtonClick(View v){
+    public void onButtonClick(View v) {
 
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText num1=(EditText)findViewById(R.id.num);
-        EditText num2=(EditText)findViewById(R.id.num2);
-        TextView Result=(TextView)findViewById(R.id.Result);
 
-            a = Integer.parseInt(num1.getText().toString());
-            b = Integer.parseInt(num2.getText().toString());
-            Log.d(TAG, "Найдём НОД чисел "+a+" "+b);
-            try {
-                while(a!=0&&b!=0) {
-                    if(a>b)
-                        a%=b;
-                    else
-                        b%=a;
+        EditText num1 = (EditText) findViewById(R.id.num);
+        EditText num2 = (EditText) findViewById(R.id.num2);
 
-                }
-                Log.i(TAG,"Результат: "+(a+b));
-            } catch (Exception e) {
-                Log.d(TAG,"Ошибка в вычислении, хотя хз какая может быть ошибка)");
-            }
+        a = Integer.parseInt(num1.getText().toString());
+        b = Integer.parseInt(num2.getText().toString());
+        Log.d(TAG, "Найдём НОД чисел " + a + " " + b);
 
+            nod=result.resNod(a,b);
 
-        String message=Integer.toString(a+b);
-        Result.setText(Integer.toString(a+b));
+        String message = Integer.toString(nod);
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+        result.showList();
 
     }
 
+    public class NOD {
+        private LinkedList<Integer> List = new LinkedList<Integer>();
+
+        public int resNod(int a, int b) {
+            List.add(a);
+            List.add(b);
+            while (a != 0 && b != 0) {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+                List.add(a+b);
+                return a+b;
+
+        }
+        public void showList(){
+                for(int i=0;i<List.size();i+=3) {
+                    Log.d(TAG, " num1 = " + List.get(i) + " num2 = " + List.get(i + 1) + " answer = " + List.get(i + 2));
+                }
+            }
+
+        public void clearList(){
+            List.clear();
+        }
+
+
+    }
 }
